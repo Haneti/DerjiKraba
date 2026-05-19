2ce1cdcfbe73741a4e6be9db134b6b9eb852bd3d
Haneti <vlad.72791@gmail.com>
2026-05-19 13:53:02 +1000

    other files

--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee239b080cfcb1068ef44176fff121ae4db4a0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cd84519954f761b2a0f6c7ee3b584d579a54f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
